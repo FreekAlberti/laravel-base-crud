124d7cf895c1c4e6679eefc37d44820e6a62923a
FreekAlberti <federicoalberi96@gmail.com>
2020-10-27 17:59:42 +0100

    wip

---
+++ resources/views/create.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form mrthod="POST" action="">
+        @csrf
+        @method("POST")
+        <div>
+            <label for="name">Titolo</label>
+            <input type="text" name="title" id="name" placeholder="Titolo">
+        </div>
+
+        <div>
+            <label for="author">Autore</label>
+            <input type="text" name="author" id="author" placeholder="Autore">
+        </div>
+
+        <div>
+            <label for="edition">Edizione</label>
+            <input type="text" name="edition" id="edition" placeholder="Edizione">
+        </div>
+
+        <div>
+            <label for="genre">Genere</label>
+            <input type="text" name="genre" id="genre" placeholder="Genere">
+        </div>
+
+        <div>
+            <label for="image">Url Immagine</label>
+            <input type="text" name="image" id="image" placeholder="Url Immagine">
+        </div>
+
+        <div>
+            <label for="year">Anno</label>
+            <input type="date" name="year" id="year" placeholder="Anno">
+        </div>
+
+        <div>
+            <label for="isbn">ISBN</label>
+            <input type="text" name="isbn" id="isbn" placeholder="ISBN">
+        </div>
+
+        <input type="submit" value="Salva">
+    </form>
+</body>
+</html>
