84c397bc2c4e58322e4fffab58a034d875ffd2da
FreekAlberti <federicoalberi96@gmail.com>
2020-10-28 14:51:24 +0100

    Wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,14 @@
     <title>Document</title>
 </head>
 <body>
-    @foreach ($books as $book)
-        
-    @endforeach
+        @foreach ($books as $book)
+            <div>{{$book->title}}</div>
+            <div>{{$book->author}}</div>
+            <div>{{$book->edition}}</div>
+            <div>{{$book->genre}}</div>
+            <img style="height: 100px; width: 100px;" src="{{$book->image}}" alt="">
+            <div>{{$book->year}}</div>
+            <div>{{$book->isbn}}</div>
+        @endforeach
 </body>
 </html>
